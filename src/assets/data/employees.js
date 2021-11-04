55fc4773d356112e07f1fbc130c4ccc8c91b95cb
Juan David <jdoo1115@gmail.com>
2021-11-04 04:42:31 -0500

    pagina nosotros acabada

---
+++ src/assets/data/employees.js
@@ -0,0 +1,32 @@
+export const employees = [
+  {
+    id: 0,
+    name: 'Juan Manuel Gomez B.',
+    job: 'Chef Ejecutivo'
+  },
+  {
+    id: 1,
+    name: 'Ana Isabel Casallas B.',
+    job: 'Chef Pastelera'
+  },
+  {
+    id: 2,
+    name: 'Maria Paulina Veloza G.',
+    job: 'Administradora'
+  },
+  {
+    id: 3,
+    name: 'Juliana Restrepo B.',
+    job: 'Chef Auxiliar'
+  },
+  {
+    id: 4,
+    name: 'Manuela Orozco C.',
+    job: 'Contadora'
+  },  
+  {
+    id: 5,
+    name: 'Daniela Berrio.',
+    job: 'Recepcionista'
+  }
+]
